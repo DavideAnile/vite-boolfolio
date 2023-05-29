4b93dbe9277b3b5f93f4d824cf9f917e97a10950
DavideAnile <davide.anile16@gmail.com>
2023-05-29 15:52:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+
+  components : {
+
+    AppHeader,
+    AppFooter,
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader></AppHeader>
+
+  <div>
+    <h1>ciao</h1>
+  </div>
+
+  <AppFooter></AppFooter>
+  
+</template>
+
+<style scoped>
+
+</style>
